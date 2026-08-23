6319397896660bc0a6e305eee6f0e1b5caa19a46
Azamat <azamat-karimov-05@mail.ru>
2026-08-24 00:38:32 +0300

    init repository

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+// Импорт типов для корректной работы TypeScript
+/**
+ * @type {import('lint-staged').Configuration}
+ */
+
+const config = {
+    "*.{js,jsx,ts,tsx,mjs,cjs}": ["eslint --fix", "prettier --write"],
+}
+
+export default config
